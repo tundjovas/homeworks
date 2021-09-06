075fbbd3e50cd6d2e14c18859fc4c9ffd191a593
Simona <simona.tundjova@gmail.com>
2021-09-06 19:06:47 +0200

    Remove unnecessary

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(require('./routes/web'));
-app.use(express.static(__dirname + '/public')); 
 
 app.listen(3000, function () {
 });
